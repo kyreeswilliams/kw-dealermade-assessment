0f561c208940882a7d239b9acd977aa1b0c97cb7
Kyree S. Williams <kyreewilliams@MacBook-Pro.local>
2016-10-27 17:57:20 -0500

    css fix to center loader

--- index.php
+++ index.php
@@ -25,9 +25,9 @@
 	</head>
 	<body ng-app="dealermadeDemo">
 		<div id="loader" ng-show="loading">
-		    <div class="spinner">
-			  <div class="double-bounce1"></div>
-			  <div class="double-bounce2"></div>
+			<div class="spinner">
+				<div class="double-bounce1"></div>
+				<div class="double-bounce2"></div>
 			</div>
 		</div>
 		<header ng-controller="infoController">
